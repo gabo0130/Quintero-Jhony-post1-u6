16e7358b1fd394d2f3e1946b3a212dca1091d818
Jhony Quintero <54184615+gabo0130@users.noreply.github.com>
2026-04-26 23:06:36 -0500

    Initial commit

---
+++ refactoring-lab/src/main/java/com/universidad/antipatrones/refactoring_lab/RefactoringLabApplication.java
@@ -0,0 +1,13 @@
+package com.universidad.antipatrones.refactoring_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactoringLabApplication.class, args);
+	}
+
+}
---
+++ refactoring-lab/src/test/java/com/universidad/antipatrones/refactoring_lab/RefactoringLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidad.antipatrones.refactoring_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
